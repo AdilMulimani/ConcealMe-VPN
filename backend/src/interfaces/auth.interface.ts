746d351972e79f43c62ea0ea7e059f1adbc04341
AdilMulimani <adilmulimani@gmail.com>
2025-06-18 20:06:14 +0530

    feat(backend/src/interfaces): add auth.interface.ts

---
+++ backend/src/interfaces/auth.interface.ts
@@ -0,0 +1,35 @@
+import {UUID} from "node:crypto";
+import {Request} from "express";
+
+export interface SignUpBody{
+name:string,
+email:string,
+password:string
+}
+
+export interface VerifyEmailBody {
+    verificationToken:string
+}
+
+export interface SignInBody{
+    email:string,
+    password:string
+}
+
+export interface ForgotPasswordBody{
+    email:string,
+}
+
+export interface VerifyResetTokenBody {
+    resetPasswordToken:string
+}
+
+export interface ResetPasswordBody {
+    resetPasswordToken:string,
+    password:string
+}
+
+export interface AuthRequest extends Request {
+    userId?: number;
+    token?:string
+}
