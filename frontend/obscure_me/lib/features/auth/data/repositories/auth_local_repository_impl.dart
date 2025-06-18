15751ef127f53b91b689bbc732a80130ae40124d
AdilMulimani <adilmulimani@gmail.com>
2025-06-18 21:04:12 +0530

    feat(frontend/lib/features/auth/data/repositories): add auth_local_repository_impl.dart

---
+++ frontend/obscure_me/lib/features/auth/data/repositories/auth_local_repository_impl.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/foundation.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:obscure_me/core/error/failure.dart';
+import 'package:obscure_me/features/auth/data/datasources/auth_local_data_source.dart';
+
+import '../../domain/repositories/local_auth_repository.dart';
+
+class AuthLocalRepositoryImpl implements AuthLocalRepository {
+  final AuthLocalDataSource authLocalDataSource;
+  const AuthLocalRepositoryImpl({required this.authLocalDataSource});
+
+  // TOKEN
+
+  // LOCK
+  @override
+  Future<void> deleteLockPattern([String key = 'LOCK_PATTERN_KEY']) async {
+    await authLocalDataSource.deleteLockPattern(key);
+  }
+
+  @override
+  Future<Either<Failure, List<int>?>> getLockPattern([
+    String key = 'LOCK_PATTERN_KEY',
+  ]) async {
+    try {
+      final lockPattern = await authLocalDataSource.getLockPattern(key);
+      return right(lockPattern);
+    } catch (e) {
+      debugPrint(e.toString());
+      return left(Failure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> saveLockPattern(List<int> pattern) async {
+    try {
+      final isPatternSet = await authLocalDataSource.saveLockPattern(pattern);
+      return right(isPatternSet);
+    } catch (e) {
+      debugPrint(e.toString());
+      return left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> verifyLockPattern(
+    List<int> pattern, [
+    String key = 'LOCK_PATTERN_KEY',
+  ]) async {
+    try {
+      final lockPattern = await authLocalDataSource.verifyLockPattern(pattern);
+      return right(lockPattern);
+    } catch (e) {
+      debugPrint(e.toString());
+      return left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> containsLockPattern([
+    String key = 'LOCK_PATTERN_KEY',
+  ]) async {
+    try {
+      final lockPattern = await authLocalDataSource.containsLockPattern(key);
+      return right(lockPattern);
+    } catch (e) {
+      debugPrint(e.toString());
+      return left(Failure(e.toString()));
+    }
+  }
+}
