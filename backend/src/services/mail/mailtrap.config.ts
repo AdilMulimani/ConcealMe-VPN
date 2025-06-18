174457a3f3dcc86ad776f66520339e86c45389b2
AdilMulimani <adilmulimani@gmail.com>
2025-06-18 20:13:26 +0530

    feat(backend/src/services/mail): add mailtrap.config.ts

---
+++ backend/src/services/mail/mailtrap.config.ts
@@ -0,0 +1,15 @@
+// import{MailtrapClient}  from "mail";
+// import dotenv from "dotenv";
+//
+// dotenv.config();
+//
+// //Create client
+// export const client = new MailtrapClient({
+//     token:String(process.env.MAILTRAP_TOKEN),
+// });
+//
+// //Create sender
+// export const sender ={
+//     email:String(process.env.SENDER_EMAIL),
+//     name:"Super Travel App",
+// }
